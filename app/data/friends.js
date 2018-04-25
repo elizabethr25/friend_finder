3912db7f853cfc1cccd491aa6bcc85a6d10dcb66
Elizabeth Richardson <emrichardson820@gmail.com>
2018-04-25 10:03:55 -0700

    friends var added

---
+++ app/data/friends.js
@@ -0,0 +1,100 @@
+//FRIENDS DATA
+//===========================================
+
+var friends = [
+    {
+        "name":"Rachel Green",
+        "photo": "#",
+        "scores":[
+            4,
+            3,
+            5,
+            1,
+            2,
+            3,
+            4,
+            2,
+            3,
+            5
+        ]
+    },
+    {
+        "name":"Monica Geller",
+        "photo":"#",
+        "scores":[
+            3,
+            4,
+            5,
+            4,
+            2,
+            2,
+            5,
+            4,
+            1,
+            5
+        ]
+    }, 
+    {
+        "name":"Phoebe Buffay",
+        "photo":"#",
+        "scores":[
+            5,
+            1,
+            1,
+            2,
+            1,
+            1,
+            1,
+            2,
+            5
+        ]
+    },
+    {
+        "name":"Chandler Bing",
+        "photo":"#",
+        "scores":[
+            2,
+            5,
+            2,
+            4,
+            4,
+            4,
+            4,
+            4,
+            2,
+            3
+        ]
+    },
+    {
+        "name":"Ross Geller",
+        "photo":"#",
+        "scores":[
+            4,
+            5,
+            2,
+            4,
+            2,
+            1,
+            5,
+            4,
+            1,
+            4
+        ]
+    },
+    {
+        "name":"Joey Tribbiani",
+        "photo":"#",
+        "scores":[
+            5,
+            1,
+            4,
+            1,
+            4,
+            4,
+            1,
+            1,
+            5,
+            5
+        ]
+    }
+]
